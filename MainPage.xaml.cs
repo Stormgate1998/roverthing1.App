2deb5d2ddc63c2ca22e2125e926e35a57392db12
Joshua Barlow <jalapenojosh@gmail.com>
2023-02-01 16:29:30 -0700

    removed unnecessary things

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,24 +13,13 @@ namespace roverthing1;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
 
-    public MainPage()
+    public MainPage(JoinViewModel model)
     {
         InitializeComponent();
+        BindingContext= model;
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
 }
 
 public partial class JoinViewModel : ObservableObject
